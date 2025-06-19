34122a173b340d16d327459cbf3feda35e451cf8
nirmal2010 <nirmalrajaa@klyonix.com>
2025-06-19 13:16:10 +0530

    Chores: Renamed label of the custom buttoon in Sales Invoice and delivery note

--- erpnext_eshipz/erpnext_eshipz/custom/delivery_note/delivery_note.js
+++ erpnext_eshipz/erpnext_eshipz/custom/delivery_note/delivery_note.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Delivery Note", {
 	refresh: function(frm) {
 		if (!frm.doc.is_return && frm.doc.status !== "Closed" && frm.doc.docstatus === 1) {
 			if (frappe.model.can_create("Shipment")) {
-				frm.add_custom_button(__("Custom Shipment"), function () {
+				frm.add_custom_button(__("Eshipz Shipment"), function () {
 					frappe.model.open_mapped_doc({
 						method: "erpnext_eshipz.erpnext_eshipz.custom.delivery_note.delivery_note.custom_make_shipment", 
 						frm: frm
